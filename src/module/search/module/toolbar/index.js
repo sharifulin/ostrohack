ae22395db986a67cdfe5ee0bcbb486e92fb1765c
Roman Dvornov <rdvornov@gmail.com>
2013-03-02 02:58:44 +0400

    search result sorting buttons

--- src/module/search/module/toolbar/index.js
+++ src/module/search/module/toolbar/index.js
@@ -1,9 +1,52 @@
 basis.require('basis.l10n');
 basis.require('basis.ui');
 
-basis.l10n.createDictionary('app.module.toolbar', __dirname + 'l10n', {
+var namespace = 'app.module.search.toolbar';
+
+basis.l10n.createDictionary(namespace, __dirname + 'l10n', {
+});
+
+basis.l10n.createDictionary(namespace + '.sorting', __dirname + 'l10n', {
+  popularity: 'popularity',
+  rating: 'rating',
+  price: 'price',
+  distance: 'distance'
+});
+
+var sorting = new basis.ui.Node({
+  template: resource('template/sorting.tmpl'),
+
+  selection: true,
+  childClass: {
+    template: resource('template/sortingButton.tmpl'),
+    binding: {
+      title: 'title'
+    }
+  },
+  childNodes: [
+    {
+      title: basis.l10n.getToken(namespace, 'sorting', 'popularity'),
+      sortFn: basis.getter('data.title'),
+      selected: true
+    },
+    {
+      title: basis.l10n.getToken(namespace, 'sorting', 'rating'),
+      sortFn: basis.getter('data.title')
+    },
+    {
+      title: basis.l10n.getToken(namespace, 'sorting', 'price'),
+      sortFn: basis.getter('data.title')
+    },
+    {
+      title: basis.l10n.getToken(namespace, 'sorting', 'distance'),
+      sortFn: basis.getter('data.title')
+    }
+  ]
 });
 
 module.exports = new basis.ui.Node({
-  template: resource('template/view.tmpl')
+  template: resource('template/view.tmpl'),
+  binding: {
+    sorting: sorting
+  }
 });
