80686a3e6b6f66de1cb44e0c681ca991fa7c5587
Roman Dvornov <rdvornov@gmail.com>
2013-03-02 06:33:49 +0400

    make city filter works

--- src/module/search/module/filters/module/cities/index.js
+++ src/module/search/module/filters/module/cities/index.js
@@ -6,6 +6,17 @@ basis.l10n.createDictionary('app.module.search.filters.cities', __dirname + 'l10
   title: 'City of region'
 });
 
+var cityMerge = new basis.data.dataset.Merge({
+  handler: {
+    sourcesChanged: function(){
+      if (this.sources.length)
+        app.search.output.addSource(this);
+      else
+        app.search.output.removeSource(this);      
+    }
+  }
+});
+
 module.exports = new basis.ui.Node({
   template: resource('template/list.tmpl'),
   
@@ -17,11 +28,28 @@ module.exports = new basis.ui.Node({
         return node.delegate.itemCount;
       }
     },
+    action: {
+      check: function(event){
+        if (event.sender.checked)
+          this.select();
+        else
+          this.unselect();
+      }
+    },
+
     listen: {
       delegate: {
         datasetChanged: function(){
           this.updateBind('count');
         }
+      }
+    },
+    handler: {
+      select: function(){
+        cityMerge.addSource(this.delegate);
+      },
+      unselect: function(){
+        cityMerge.removeSource(this.delegate);
       }
     }
   },
