76c9ad8c3c8fa271c9c29136c6b32aca4c4bf985
Roman Dvornov <rdvornov@gmail.com>
2013-03-02 07:24:18 +0400

    make aminities filters works

--- src/module/search/module/filters/module/amenities/index.js
+++ src/module/search/module/filters/module/amenities/index.js
@@ -1,4 +1,5 @@
 basis.require('basis.l10n');
+basis.require('basis.data.dataset');
 basis.require('basis.ui');
 
 var namespace = 'app.module.search.filters.amenities';
@@ -10,6 +11,22 @@ basis.l10n.createDictionary(namespace, __dirname + 'l10n', {
   parking: 'parking'
 });
 
+var splitByAmenities = new basis.data.dataset.Cloud({
+  source: app.search.input,
+  rule: 'data.amenities'
+});
+var amenitiesMerge = new basis.data.dataset.Merge({
+  rule: basis.data.dataset.Merge.INTERSECTION,
+  handler: {
+    sourcesChanged: function(){
+      if (this.sources.length)
+        app.search.output.addSource(this);
+      else
+        app.search.output.removeSource(this);      
+    }
+  }
+});
+
 module.exports = new basis.ui.Node({
   template: resource('template/list.tmpl'),
   
@@ -18,21 +35,41 @@ module.exports = new basis.ui.Node({
     binding: {
       title: 'data:',
       name: 'data:'
+    },
+    action: {
+      check: function(event){
+        if (event.sender.checked)
+          this.select();
+        else
+          this.unselect();
+      }
+    },
+
+    handler: {
+      select: function(){
+        amenitiesMerge.addSource(this.data.dataset);
+      },
+      unselect: function(){
+        amenitiesMerge.removeSource(this.data.dataset);
+      }
     }
   },
 
   childNodes: basis.data([
     {
       name: 'internet',
-      title: basis.l10n.token(namespace, 'internet')
+      title: basis.l10n.token(namespace, 'internet'),
+      dataset: splitByAmenities.getSubset('internet', true)
     },
     {
       name: 'breakfast',
-      title: basis.l10n.token(namespace, 'breakfast')
+      title: basis.l10n.token(namespace, 'breakfast'),
+      dataset: splitByAmenities.getSubset('breakfast', true)
     },
     {
       name: 'parking',
-      title: basis.l10n.token(namespace, 'parking')
+      title: basis.l10n.token(namespace, 'parking'),
+      dataset: splitByAmenities.getSubset('parkovka', true)
     }
   ])
 });
