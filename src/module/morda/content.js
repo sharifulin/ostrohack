3b25339de106555c00da773f05686b5d9003287a
Anatoly Sharifulin <sharifulin@gmail.com>
2013-03-03 02:45:51 +0400

    l10n banners, it is hard :)

--- src/module/morda/content.js
+++ src/module/morda/content.js
@@ -1,6 +1,21 @@
 
   basis.require('basis.ui');
+  
+  var namespace = 'app.module.morda.banners';
+  
+  basis.l10n.createDictionary(namespace + '.b1', __dirname + 'l10n', {
+    title: 'Easy to use iPhone app',
+    info: 'Book hotels wherever you are via your iPhone',
+    tip: 'type ostrovok'
+  });
 
+  basis.l10n.createDictionary(namespace + '.b2', __dirname + 'l10n', {
+    title: 'Title',
+    label1: 'Customer Support',
+    label2: 'Service',
+    info: 'Info'
+  });
+  
   var SearchForm = resource('module/searchForm/index.js').fetch();
 
   module.exports = new basis.ui.Node({
