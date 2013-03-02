56f3ac3d051fefaf1a963da1b1e887ee0989edf2
wuzyk <vratsev@gmail.com>
2013-03-03 05:11:06 +0600

    add button to slider

--- src/module/hotel/module/slider/index.js
+++ src/module/hotel/module/slider/index.js
@@ -11,9 +11,22 @@ var templates = basis.template.define('app.module.hotel.slider', {
 module.exports = basis.ui.scroller.ScrollPanel.subclass({
   scrollX: true,
   scrollY: false,
-  wheelDelta: 100,
+  wheelDelta: 150,
+
 
   template: templates.imageList,
+
+  action: {
+    onwheel: function(){},
+    scrollLeft: function(event){
+      this.scroller.setPositionX(this.scroller.viewportTargetX - this.wheelDelta * 4, true);
+      basis.dom.event.kill(event.event_);
+    },
+    scrollRight: function(event){
+      this.scroller.setPositionX(this.scroller.viewportTargetX + this.wheelDelta * 4, true);
+      basis.dom.event.kill(event.event_);
+    }
+  },
 
   childClass: {
     template: templates.imageItem,
