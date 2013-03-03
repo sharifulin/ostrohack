7e048dd52ed7886eab74db1b051f15ed6d333d69
Roman Dvornov <rdvornov@gmail.com>
2013-03-03 23:44:29 +0400

    check theme on page load

--- src/app/settings/theme.js
+++ src/app/settings/theme.js
@@ -4,10 +4,15 @@ basis.require('basis.dom.event');
 basis.template.define('app', resource('../template/index.js').fetch());
 basis.template.theme('mobile').define('app', resource('../template/theme-mobile/index.js').fetch());
 
-// handle screen changes
-basis.dom.event.addHandler(window, 'resize', function(){
+
+function checkTheme(){
   if (window.innerWidth <= 480)
     basis.template.setTheme('mobile');
   else
     basis.template.setTheme('base');
-});
+}
+
+checkTheme();
+
+// handle screen changes
+basis.dom.event.addHandler(window, 'resize', checkTheme);
