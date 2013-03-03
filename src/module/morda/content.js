fa89c4069f3e7230853818fdca440eef86d436a7
Anatoly Sharifulin <sharifulin@gmail.com>
2013-03-03 19:42:31 +0400

    morda changes whith theme

--- src/module/morda/content.js
+++ src/module/morda/content.js
@@ -2,23 +2,15 @@
   basis.require('basis.ui');
   basis.require('app.ext');
   
-  var namespace = 'app.module.morda.banners';
+  resource('settings/l10n.js').fetch();
   
-  basis.l10n.createDictionary(namespace + '.b1', __dirname + 'l10n', {
-    title: 'Easy to use iPhone app',
-    info: 'Book hotels wherever you are via your iPhone',
-    tip: 'type ostrovok'
-  });
-
-  basis.l10n.createDictionary(namespace + '.b2', __dirname + 'l10n', {
-    title: 'Title',
-    label1: 'Customer Support',
-    label2: 'Service',
-    info: 'Info'
-  });
+  var namespace = 'app.module.morda';
+  
+  var templates = basis.template.define(namespace, resource('template/index.js').fetch());
+  basis.template.theme('mobile').define(namespace, resource('template/theme-mobile/index.js').fetch());
   
   module.exports = new basis.ui.Node({
-    template: resource('template/view.tmpl'),
+    template: templates.view,
     binding: {
       form: new app.ext.SearchForm({}),
       content: resource('module/content/index.js').fetch()
---
+++ src/module/morda/settings/l10n.js
@@ -0,0 +1,14 @@
+var namespace = 'app.module.morda.banners';
+  
+basis.l10n.createDictionary(namespace + '.b1', __dirname + 'l10n', {
+  title: 'Easy to use iPhone app',
+  info: 'Book hotels wherever you are via your iPhone',
+  tip: 'type ostrovok'
+});
+
+basis.l10n.createDictionary(namespace + '.b2', __dirname + 'l10n', {
+  title: 'Title',
+  label1: 'Customer Support',
+  label2: 'Service',
+  info: 'Info'
+});
